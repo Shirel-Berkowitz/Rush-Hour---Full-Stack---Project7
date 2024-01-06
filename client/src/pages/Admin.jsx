f4bf2d134d5d19f7dec09a8aa8dd52663257ae6a
Shirel <shkadosh@g.jct.ac.il>
2024-01-06 21:52:50 +0200

    navbar design

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -8,22 +8,69 @@ const Admin = () => {
   var user = JSON.parse(localStorage.getItem("currentUser"));
   return (
     <div className="users-container">
-      <h1 className="user-name">welcome admin {user.name}</h1>
-      <Link
-        to="/Login"
-        style={{ position: "absolute", top: 0, right: 0, margin: "10px" }}
-      >
-        <button
-          className="logout-button"
-          onClick={() => {
-            localStorage.removeItem("currentUser");
-          }}
-        >
-          <FontAwesomeIcon icon={faSignOutAlt} />
-        </button>
-      </Link>
+      <header>
+        <nav className="top-navbar">
+          <ul className="nav__links">
+            <li>
+              <h2 className="logo">Rush hour</h2>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/Register`}>Add user</Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/UserList`}>Users</Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/camerasList`}>Cameras</Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/CamerasAccess`}>
+                Cameras permissions
+              </Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/UserAccess`}>Users permissions</Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/UnusualEvents`}>Unusual Events</Link>
+            </li>
+            <li>
+              <Link to={`/Admin/${user.id}/CamerasAdmin`}>My Cameras</Link>
+            </li>
+            {/* Uncomment the following lines when needed */}
+            {/* <li>
+            <Link to={`/Admin/${user.id}/VideoAnalysis`}>Video analysis</Link>
+          </li> */}
+            <li>
+              <Link to={`/Admin/${user.id}/Info`}>Info</Link>
+            </li>
+            <li>
+              <Link
+                to="/Login"
+                // style={{
+                //   position: "absolute",
+                //   top: 0,
+                //   right: 0,
+                //   margin: "10px",
+                // }}
+              >
+                <button
+                  className="logout-button"
+                  onClick={() => {
+                    localStorage.removeItem("currentUser");
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-      <Link to={`/Admin/${user.id}/Register`}>
+      <h1 className="user-name">welcome admin {user.name}</h1>
+
+      {/* <Link to={`/Admin/${user.id}/Register`}>
         <button>Add new user</button>
       </Link>
       <Link to={`/Admin/${user.id}/UserList`}>
@@ -40,7 +87,7 @@ const Admin = () => {
       </Link>
       <Link to={`/Admin/${user.id}/UnusualEvents`}>
         <button>Unusual Events</button>
-      </Link>
+      </Link> */}
 
       <nav className="user-navigation">
         <ul>
